230dd7ad803ceb191c6f0efae46c0c555fb368ba
adagiomedia <adagiomediarus@gmail.com>
2024-09-17 11:07:02 +0200

    Upload 17.09
    cf7_coinsnap-styles.css is removed

--- coinsnap-for-contactform7.php
+++ coinsnap-for-contactform7.php
@@ -9,7 +9,7 @@
  * Text Domain:     coinsnap-for-contactform7
  * Domain Path:     /languages
  * Requires PHP:    7.4
- * Tested up to:    6.6.1
+ * Tested up to:    6.6.2
  * Requires at least: 5.2
  * License:         GPL2
  * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
